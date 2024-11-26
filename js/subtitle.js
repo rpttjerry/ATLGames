bf82e61aadebd6289ebb8031ac4f954d8d3ef8db
rpttjerry <158204613+rpttjerry@users.noreply.github.com>
2024-11-26 08:46:33 -0800

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -6,10 +6,8 @@ function changeSub(num) {
 
 
 var say = [];
-say[0] = "no more free dthang";
-say[1] = "50$ for 4 methods tap in wit me @rptjerry";
-say[2] = "free lee drilly";
-say[3] = "tap in wit me for motion @rptjerry"
+say[0] = "tap in 50$ for 4 methods to get motion @rptjerry";
+say[1] = "50$ for 4 methods tap in @rptjerry";
 // pick a random greeting
 var howmany = say.length;
 var bRand = 0;
